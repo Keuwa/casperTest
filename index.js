f4f97b49a75eeb09b3d4db782247084917c4c600
Keuwa <vincent.durpoix@gmail.com>
2017-01-18 17:55:34 +0100

    json output ok

--- index.js
+++ index.js
@@ -1,75 +1,70 @@
 var _ = require('lodash')
-var page = require('webpage').create(),
-  system = require('system'),
+var webpage = require('webpage')
+//var page = require('webpage').create(),
+var system = require('system'),
   t ;
- var fs = require('fs');
+var fs = require('fs');
+var Promise = require('promise-polyfill');
 
 var content = fs.read('siteList.csv');
 var addresses = content.split("\n");
 
 
-var urlList = {};
-
-console.log("heys");
 
 
 
+function testLoading (address,resolve) {
+  return new Promise(function(resolve, reject) {
+    result={}
 
+    address = address.replace(/\r|\n/g,'');
+    result.requestsTable = []
+    t = Date.now();
+    page = webpage.create();
 
-function testLoading (address, resolve) {
-  // address = address.replace(/\r|\n/g,'');
-  //
-  // console.log("adress : ", JSON.stringify(address));
-  // urlList[address]=[]
-  // urlList[address].requestsTable = []
-  //
-  // t = Date.now();
-  //
-  // page.onResourceRequested = function(request) {
-  //   if(_.includes(request.url,"zebestof")){
-  //     urlList[address].requestsTable.push({
-  //                             "url":request.url,
-  //                             "time":Date.now()
-  //                         })
-  //   }
-  // };
-  //
-  // page.onResourceReceived = function(response) {
-  //   if(_.includes(response.url,"zebestof")){
-  //     for (var i = 0;i < urlList[address].requestsTable.length;i++) {
-  //       if(_.includes(urlList[address].requestsTable[i].url,response.url)){
-  //         urlList[address].requestsTable[i].response = {"url":response.url,"time":Date.now()}
-  //         console.log("received:",JSON.stringify(urlList[address].requestsTable));
-  //       }
-  //     }
-  //   }
-  // };
-  //
-  // page.open(address, function(status) {
-  //   if (status !== 'success') {
-  //     console.log('FAIL to load the address '+status);
-  //   } else {
-  //     t = Date.now() - t;
-  //     console.log('Loading ' + address);
-  //     console.log('Loading time ' + t + ' msec');
-  //   }
-  //   phantom.exit();
-  //   resolve();
-  // });
-}
+    page.onResourceRequested = function(request) {
+      if(_.includes(request.url,"zebestof")){
+        //console.log(request.url,"onResourceRequested");
+        result.requestsTable.push({
+                                "url":request.url,
+                                "time":Date.now() - t
+                            })
+      }
+    };
 
+    page.onResourceReceived = function(response) {
+      if(_.includes(response.url,"zebestof")){
+        //console.log(response.url,"onResourceReceived");
+        for (var i = 0;i < result.requestsTable.length;i++) {
+          //console.log(_.includes(result.requestsTable[i].url,response.url));
+          if(_.includes(result.requestsTable[i].url,response.url)){
+            result.requestsTable[i].response = {"url":response.url,"time":Date.now() - t}
+          }
+        }
+      }
+    };
 
-var promises[];
+    page.open(address, function(status) {
+      if (status !== 'success') {
+        console.log('FAIL to load the address : ',address);
+      } else {
+        var obj = {}
+        obj[address] = []
+        obj[address].push(result)
+        obj[address].push({"startLoading":t});
+        //t = Date.now() - t;
+        //console.log('Loading ' + address);
+        //console.log('Loading time ' + t + ' msec');
+        setTimeout(resolve(obj),5000)
+        //console.log("supose to be at the end");
+      }
+    });
 
-for(i in addresses){
-  promises.push(new Promise(function(resolve, reject) {
-    testLoading(adresses[i]),resolve)
   });
 }
 
-Promise.all(promises).then((valeurs) => {
-  console.log(valeurs);
-  console.log("\n\n\n yolo : ",JSON.stringify(urlList));
-}, (raison) => {
-  console.log(raison)
+var urlList = {};
+Promise.all(addresses.map(testLoading)).then(function (results) {
+  console.log(JSON.stringify(results));
+  phantom.exit();
 });
